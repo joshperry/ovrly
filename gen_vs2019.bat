44c74c1ba5851b5296a7bb3953b91d9266348ee9
Josh Perry <josh@6bit.com>
2020-04-10 22:08:19 -0600

    Move CEF_ROOT out of gen_vs2019.bat helper

--- gen_vs2019.bat
+++ gen_vs2019.bat
@@ -1,6 +1,8 @@
 @echo off
 
-set CEF_ROOT=h:\dev\cef_binary_80.1.15+g7b802c9+chromium-80.0.3987.163_windows64
+rem set CEF_ROOT environment variable
+rem add it to your user environment if you want to take advantage of VS cmake integration
+rem CEF_ROOT=h:\dev\cef_binary_80.1.15+g7b802c9+chromium-80.0.3987.163_windows64
 
 set BASE_DIR=%~dp0
 rem echo %BASE_DIR%
@@ -9,7 +11,7 @@ mkdir "%BASE_DIR%\build"
 
 cd "%BASE_DIR%\build"
 
-rem use "Visual Studio 15" as the generator for x86 builds
+rem use Visual Studio as the generator
 rem cmake -G "Visual Studio 15" "%BASE_DIR%"
 cmake -G "Visual Studio 16 2019" -A x64 "%BASE_DIR%"
 
