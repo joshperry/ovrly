dbabf5f212afa0abec2db1a4d8b26c8b534dd479
Josh Perry <josh@6bit.com>
2020-05-22 10:53:37 -0600

    First whack at ci on circle

---
+++ .circleci/build.ps1
@@ -0,0 +1,43 @@
+$ProgressPreference = "SilentlyContinue"
+$ErrorActionPreference="Stop"
+
+######
+# Install external dependencies
+######
+
+# Install cmake
+$CMAKE_VERSION=3.17.2
+Invoke-WebRequest -URI https://github.com/Kitware/CMake/releases/download/v$CMAKE_VERSION/cmake-$CMAKE_VERSION-win64-x64.zip -OutFile $Env:HOMEPATH\cmake-$CMAKE_VERSION-win64-x64.zip
+Expand-Archive $Env:HOMEPATH\cmake-$CMAKE_VERSION-win64-x64.zip -DestinationPath "$Env:ProgramFiles"
+Rename-Item "$Env:ProgramFiles\cmake-$CMAKE_VERSION-win64-x64" -NewName CMake
+
+if (-not (Get-Command cmake -ErrorAction SilentlyContinue)) {
+    New-Alias -Name cmake -Value "$Env:ProgramFiles\CMake\bin\cmake.exe"
+}
+
+# Get mathfu
+Push-Location $Env:HOMEPATH
+git clone --recursive https://github.com/google/mathfu.git
+
+$MATHFU_ROOT=$Env:HOMEPATH\mathfu
+
+# Get cef
+$CEF_VERSION="cef_binary_81.3.10+gb223419+chromium-81.0.4044.138_windows64"
+Invoke-WebRequest -URI http://opensource.spotify.com/cefbuilds/$CEF_VERSION.tar.bz2 -OutFile $Env:HOMEPATH\$CEF_VERSION.tar.bz2
+tar -zxvf $CEF_VERSION.tar.bz2
+
+$CEF_ROOT=$Env:HOMEPATH\$CEF_VERSION
+
+Pop-Location
+
+######
+# Build
+######
+
+# Generate visual studio sln
+gen_vs2019.bat
+
+# Execute the build
+Push-Location build
+cmake --build . --config Release
+Pop-Location
